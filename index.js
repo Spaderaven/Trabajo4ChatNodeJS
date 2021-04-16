7c0ae8c474846c8058e3cd77bd76bb4021052e5e
Choba <watsit777@gmail.com>
2021-04-16 00:52:11 -0500

    ChatV1

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var socket = require('socket.io')
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log("Escuchando");
+});
+
+// Static Files
+app.use(express.static('public'));
+
+
+// Socket Setup
+var io = socket(server);
+
+io.on('connection', function(socket) { 
+    console.log("Conexion Hecha"); 
+    console.log(socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data)
+    })
+
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    })
+
+})
+
